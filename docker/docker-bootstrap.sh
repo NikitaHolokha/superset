201fccc285d182e8d448ebf46bd595c3da4c980a
NikitaHolokha <nikita.h@data-ops.tech>
2024-03-14 10:57:52 +0200

    deploy superset

--- docker/docker-bootstrap.sh
+++ docker/docker-bootstrap.sh
@@ -15,7 +15,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-sleep 90
 set -eo pipefail
 
 REQUIREMENTS_LOCAL="/app/docker/requirements-local.txt"
